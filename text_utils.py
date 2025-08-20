28dd2967bb6fdded5685cd2a850cb12a04564b2f
Dmitriy Volkov <volkov.da25@gmail.com>
2025-08-20 16:52:19 +0300

    adding simple tokenizer

--- text_utils.py
+++ text_utils.py
@@ -1,7 +1,15 @@
 import tiktoken
 import re
 
-tokenizer = tiktoken.get_encoding("gpt2")
-text = "Hello, my name is Dima"
-tokens = tokenizer.encode(text)
-print(tokens)
+
+def tokenize(file_name, model="cl100k_base"):
+    with open(file_name, "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    tokenizer = tiktoken.get_encoding(model)
+    tokens = tokenizer.encode(raw_text)
+    return tokens
+
+if __name__ == "__main__":
+    smpl = tokenize("tolstoy.txt")
+    print(smpl[:50])
+    print(len(set(smpl)))
