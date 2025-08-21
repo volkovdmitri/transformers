47dada38c8dc2b6d9e1bb5783d6c91d3322ea631
Dmitriy Volkov <dmitriy.volkov@dskbank.bg>
2025-08-21 17:59:17 +0300

    adding multi head attention class

--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,20 @@
+import torch
+
 from text_utils import Tokenizer
 from data_preparation import create_dataloader
-import torch
+from attention import MultiHeadAttention
 
 
 if __name__ == "__main__":
     
     configs = {
-        "file_name": "tolstoy.txt",
+        "file_name": "the-verdict.txt",
         "n_tokens": None,
         "batch_size": 8,
         "context_length": 4,
         "stride": 4,
-        "embedding_size": 16
+        "embedding_size": 16,
+        "num_heads": 2
     }
 
     tokenizer = Tokenizer(configs["file_name"])
@@ -35,4 +38,12 @@ if __name__ == "__main__":
     inputs, targets = next(dataloader)
     emb_inputs = embedding_layer(inputs) + pos_embedding_layer(torch.arange(configs["context_length"]))
 
+    mha = MultiHeadAttention(
+        d_in=configs["embedding_size"], 
+        d_out=configs["embedding_size"], 
+        num_heads=configs["num_heads"], 
+        context_length=configs["context_length"], 
+        dropout=0.1)
+    
     print(emb_inputs.shape)
+    print(mha.forward(emb_inputs).shape)
